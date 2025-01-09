a7dd813d563ea08ea44aed5648eae4cbe534d539
Ylenia <yleniamighela@gmail.com>
2025-01-09 15:01:58 +0100

    [fix] add exercise 1 saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,21 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
+const userName = 'Ylenia';
 
 
 // Dichiara la funzione qui.
+
+function saluto(nome) {
+  
+    return `Ciao ${nome}`;
+
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
+console.log (saluto(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
