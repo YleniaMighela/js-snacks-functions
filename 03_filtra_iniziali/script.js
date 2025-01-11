0063d994496f34862f8336208962d6e73b861f82
Ylenia <yleniamighela@gmail.com>
2025-01-11 13:59:24 +0100

    [fix] add exercise 3 filtrainiziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,38 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function nomeLetteraA (nomeArray, lettera) {
+
+   
+    // creo un array vuoto
+    const letteraSpecifica = [];
+
+       // creo un ciclo for
+
+       for ( let i = 0; i < nomeArray.length; i++) {
+
+        if ( nomeArray[i].charAt(0) === lettera) {
+
+            letteraSpecifica.push(nomeArray[i]);
+        }
+
+
+       }
+
+
+return letteraSpecifica
+
+}
+const nomiPresenti = nomeLetteraA(names,"A");
+ console.log(nomiPresenti);
+ 
+
+
+   
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
